adbe7cb320d90a6c4180c169ff88c3b50002a4f3
dgatke17 <104190894+dgatke17@users.noreply.github.com>
2024-06-04 10:26:06 +0200

    Update installer.py

--- facefusion/installer.py
+++ facefusion/installer.py
@@ -10,22 +10,22 @@ from facefusion import metadata, wording
 from facefusion.common_helper import is_linux, is_macos, is_windows
 
 if is_macos():
-	os.environ['SYSTEM_VERSION_COMPAT'] = '0'
+    os.environ['SYSTEM_VERSION_COMPAT'] = '0'
 
-ONNXRUNTIMES : Dict[str, Tuple[str, str]] = {}
+ONNXRUNTIMES: Dict[str, Tuple[str, str]] = {}
 
 if is_macos():
-	ONNXRUNTIMES['default'] = ('onnxruntime', '1.17.3')
+    ONNXRUNTIMES['default'] = ('onnxruntime', '1.17.3')
 else:
-	ONNXRUNTIMES['default'] = ('onnxruntime', '1.17.3')
-	ONNXRUNTIMES['cuda-12.2'] = ('onnxruntime-gpu', '1.17.1')
-	ONNXRUNTIMES['cuda-11.8'] = ('onnxruntime-gpu', '1.17.1')
-	ONNXRUNTIMES['openvino'] = ('onnxruntime-openvino', '1.15.0')
+    ONNXRUNTIMES['default'] = ('onnxruntime', '1.17.3')
+    ONNXRUNTIMES['cuda-12.2'] = ('onnxruntime-gpu', '1.17.1')
+    ONNXRUNTIMES['cuda-11.8'] = ('onnxruntime-gpu', '1.17.1')
+    ONNXRUNTIMES['openvino'] = ('onnxruntime-openvino', '1.15.0')
 if is_linux():
-	ONNXRUNTIMES['rocm-5.4.2'] = ('onnxruntime-rocm', '1.16.3')
-	ONNXRUNTIMES['rocm-5.6'] = ('onnxruntime-rocm', '1.16.3')
+    ONNXRUNTIMES['rocm-5.4.2'] = ('onnxruntime-rocm', '1.16.3')
+    ONNXRUNTIMES['rocm-5.6'] = ('onnxruntime-rocm', '1.16.3')
 if is_windows():
-	ONNXRUNTIMES['directml'] = ('onnxruntime-directml', '1.17.3')
+    ONNXRUNTIMES['directml'] = ('onnxruntime-directml', '1.17.3')
 
 
 def cli() -> None:
@@ -41,6 +41,7 @@ def cli() -> None:
 
     run(args)
 
+
 def run(args) -> None:
     python_id = 'cp' + str(sys.version_info.major) + str(sys.version_info.minor)
 
@@ -48,22 +49,18 @@ def run(args) -> None:
         sys.stdout.write(wording.get('conda_not_activated') + os.linesep)
         sys.exit(1)
 
-    answers = {
-        'onnxruntime': args.onnxruntime
-    }
+    onnxruntime = args.onnxruntime
 
-    if answers:
-        onnxruntime = answers['onnxruntime']
+    if onnxruntime:
         onnxruntime_name, onnxruntime_version = ONNXRUNTIMES[onnxruntime]
 
         subprocess.call(['pip', 'install', '-r', 'requirements.txt', '--force-reinstall'])
-        if onnxruntime == 'rocm-5.4.2' or onnxruntime == 'rocm-5.6':
+        if onnxruntime in ['rocm-5.4.2', 'rocm-5.6']:
             if python_id in ['cp39', 'cp310', 'cp311']:
-                rocm_version = onnxruntime.replace('-', '')
-                rocm_version = rocm_version.replace('.', '')
-                wheel_name = 'onnxruntime_training-' + onnxruntime_version + '+' + rocm_version + '-' + python_id + '-' + python_id + '-manylinux_2_17_x86_64.manylinux2014_x86_64.whl'
+                rocm_version = onnxruntime.replace('-', '').replace('.', '')
+                wheel_name = f'onnxruntime_training-{onnxruntime_version}+{rocm_version}-{python_id}-{python_id}-manylinux_2_17_x86_64.manylinux2014_x86_64.whl'
                 wheel_path = os.path.join(tempfile.gettempdir(), wheel_name)
-                wheel_url = 'https://download.onnxruntime.ai/' + wheel_name
+                wheel_url = f'https://download.onnxruntime.ai/{wheel_name}'
                 subprocess.call(['curl', '--silent', '--location', '--continue-at', '-', '--output', wheel_path, wheel_url])
                 subprocess.call(['pip', 'uninstall', wheel_path, '-y', '-q'])
                 subprocess.call(['pip', 'install', wheel_path, '--force-reinstall'])
@@ -71,6 +68,12 @@ def run(args) -> None:
         else:
             subprocess.call(['pip', 'uninstall', 'onnxruntime', onnxruntime_name, '-y', '-q'])
             if onnxruntime == 'cuda-12.2':
-                subprocess.call(['pip', 'install', onnxruntime_name + '==' + onnxruntime_version, '--extra-index-url', 'https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple', '--force-reinstall'])
+                subprocess.call(['pip', 'install', f'{onnxruntime_name}=={onnxruntime_version}', '--extra-index-url', 'https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple', '--force-reinstall'])
             else:
-                subprocess.call(['pip', 'install', onnxruntime_name + '==' + onnxruntime_version, '--force-reinstall'])
+                subprocess.call(['pip', 'install', f'{onnxruntime_name}=={onnxruntime_version}', '--force-reinstall'])
+    else:
+        print("No action taken")
+
+
+if __name__ == "__main__":
+    cli()
